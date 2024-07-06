2763a1e75c1076714df196d29d3adc61ca7017dc
mehedee-hassan <razin.meha@gmail.com>
2024-07-06 16:40:52 +0900

    correcting for reat v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
-import { Container } from 'react-bootstrap'
-
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen  from './screens/HomeScreen';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <main className='py-3'>
-        <Container>
-          <HomeScreen/>
-        </Container>
-      </main>
-      <Footer></Footer>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} exact />
+          <Route path="/product/:id" element={<ProductScreen />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
