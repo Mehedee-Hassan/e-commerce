76e705b00efbbd341bfee3ceba5f89f91c728629
mehedee-hassan <razin.meha@gmail.com>
2024-07-06 23:30:37 +0900

    refactor

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 
+
 def getRoutes(requests):
+    routes = [
+        '/v1/api/products',
+        '/v1/api/products/create',
+        '/v1/api/products/upload'
+
+    ]
     return JsonResponse('Hello', safe=False)
