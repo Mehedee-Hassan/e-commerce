6c138119e4e5501d190121f942643ec495671d79
mehedee-hassan <razin.meha@gmail.com>
2024-07-13 12:48:39 +0900

    add urls

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,7 +7,13 @@ def getRoutes(requests):
     routes = [
         '/v1/api/products',
         '/v1/api/products/create',
-        '/v1/api/products/upload'
+        '/v1/api/products/upload',
+        '/v1/api/products/<id>/reviews/',
+        '/v1/api/products/top/',
+        '/v1/api/products/<id>/',
+
+        '/v1/api/products/delete/<id>/',
+        '/v1/api/products/<update>>/<id>/',
 
     ]
     return JsonResponse('Hello', safe=False)
